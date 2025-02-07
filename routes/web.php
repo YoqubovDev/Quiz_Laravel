aba5c962d43644c196fd07346c0fb50a7644da0e
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-07 08:49:15 +0500

    in Dashboard create-quiz, dashboard and my-quiz and Takeke-quiz folder parch:web fail

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/take-quiz', function () {
+    return view('take-quiz.take-quiz');
+});
+Route::get('/create-quiz', function () {
+    return view('dashboard.create-quiz');
+});
+Route::get('/my-quizzes', function () {
+    return view('dashboard.my-quizzes');
+});
+
