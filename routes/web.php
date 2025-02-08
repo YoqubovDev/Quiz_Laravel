dc7369d64d0840077fa01ddb41511fef622a02ae
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-08 10:43:22 +0500

    Update about and web files

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,33 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
 
-Route::get('/', function () {
-    return view('home');
+//Dashboard
+
+Route::get('/dashboard', [DashboardController::class, 'home'])->name('home');
+Route::get('/dashboard/my-quizzes', [DashboardController::class, 'my_quizzes'])->name('my-quizzes');
+Route::get('/dashboard/create-quiz', [DashboardController::class, 'create_quiz'])->name('create_quiz');
+Route::get('/dashboard/statistics', [DashboardController::class, 'statistics'])->name('statistics');
+
+//Quiz
+Route::get('/take-quiz',[QuizController::class,'take_quiz'])->middleware('auth')->name('take_quiz');
+
+//
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/register', function () {
-    return view('auth.register');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard.home');
-});
-
-Route::get('/dashboard/take-quiz', function () {
-    return view('take-quiz.take-quiz');
-});
-
-Route::get('/dashboard/create-quiz', function () {
-    return view('dashboard.create-quiz');
-});
-
-Route::get('/dashboard/my-quizzes', function () {
-    return view('dashboard.my-quizzes');
-});
-
-Route::get('/dashboard/statistics', function () {
-    return view('dashboard.statistics');
-});
-
+require __DIR__.'/auth.php';
